67a455efc7cb13372593631a2e4b13b5da69e68a
UsopKing7 <programadorusop@gmail.com>
2025-05-22 01:00:08 -0400

    interfaces y tipos para el registro y login

--- Backend/src/config.ts
+++ Backend/src/config.ts
@@ -1,7 +1,35 @@
+import { RowDataPacket } from 'mysql2'
+
 interface Port {
   port: number
 }
 
 export const PORT: Port = {
-  port: Number(process.env.PORT) ?? 3000
+  port: Number(process.env.PORT) || 3333
+}
+
+export type Login = {
+  email: string
+  password: string
+}
+
+export interface UsernameConsulta extends RowDataPacket{
+  id: `${string}-${string}-${string}-${string}-${string}`
+  username: string
+  email: string
+  password: string
+}
+
+export type Register = {
+  username: string,
+  email: string,
+  password: string
+}
+
+interface Sal {
+  sal: number
+}
+
+export const SAL: Sal = {
+  sal: Number(process.env.SAL) || 10
 }
