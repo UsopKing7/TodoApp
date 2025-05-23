ae2d3470facf2984327bbf4d456f54e7b35b5cf0
UsopKing7 <programadorusop@gmail.com>
2025-05-23 01:58:47 -0400

    agregamos la ruta de tareas

--- Backend/src/routers/middelware.ts
+++ Backend/src/routers/middelware.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express'
 import { routerLogin } from '../controllers/Inicio/login'
 import { routerUsuario } from '../controllers/usuario/usuario'
+import { tareasUsuario } from '../controllers/tareas/tareasUsuario'
 
 export const midelware = Router()
 
 midelware.use('/api', routerLogin)
 midelware.use('/api', routerUsuario)
+midelware.use('/api', tareasUsuario)
