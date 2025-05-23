70d13625ee3a2a5c1d0a75cda09ac22c413a2770
UsopKing7 <programadorusop@gmail.com>
2025-05-23 01:59:09 -0400

    valiaciones para las tareas

--- Backend/src/routers/validaciones.ts
+++ Backend/src/routers/validaciones.ts
@@ -16,3 +16,15 @@ export const updateUsuario = z.object({
   email: z.string().email().optional(),
   password: z.string().min(8, 'La contraseña debe tener al menos 8 caracteres').optional()
 })
+
+export const agregarTarea = z.object({
+  titulo: z.string().min(3, 'El titulo es requerido y no debe ser vacio o menor a 3 caracteres'),
+  descripcion: z.string().min(10, 'La descripcion es requerida y no debe ser vacio o menor a 10 caracteres'),
+  completada: z.boolean().default(false)
+})
+
+export const updateTarea = z.object({
+  titulo: z.string().min(3, 'El titulo es requerido y no debe ser vacio o menor a 3 caracteres').optional(),
+  descripcion: z.string().min(10, 'La descripcion es requerida y no debe ser vacio o menor a 10 caracteres').optional(),
+  completada: z.boolean().optional()
+})
