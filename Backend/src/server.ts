9eccf1bb79c71638298dd1a70a71dc547943a3cc
UsopKing7 <programadorusop@gmail.com>
2025-05-24 17:52:42 -0400

    use cookieParser

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -1,11 +1,13 @@
 import express from 'express'
 import { PORT } from './config'
 import { midelware } from './routers/middelware'
+import cookieParser from 'cookie-parser'
 
 const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.disable('x-powered-by')
+app.use(cookieParser())
 
 app.use(midelware)
 
