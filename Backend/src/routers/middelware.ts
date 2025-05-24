d8487c0fc2bc2f1ed61ecdcd1e710124412e41cf
UsopKing7 <programadorusop@gmail.com>
2025-05-24 17:53:42 -0400

    importacion rutaProtegida en carpetas rutas

--- Backend/src/routers/middelware.ts
+++ Backend/src/routers/middelware.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express'
 import { routerLogin } from '../controllers/Inicio/login'
+import { rutaProtegida } from '../controllers/Inicio/rutaProtected'
 import { routerUsuario } from '../controllers/usuario/usuario'
 import { tareasUsuario } from '../controllers/tareas/tareasUsuario'
 
 export const midelware = Router()
 
 midelware.use('/api', routerLogin)
-midelware.use('/api', routerUsuario)
+midelware.use('/api', rutaProtegida, routerUsuario)
 midelware.use('/api', tareasUsuario)
