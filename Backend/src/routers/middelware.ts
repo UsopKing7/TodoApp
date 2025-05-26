c873451e76a8d6f68c33bc56189fb26f7a0cb82d
UsopKing7 <programadorusop@gmail.com>
2025-05-26 02:56:28 -0400

    agrega ruta para recuperar y verificar usuario en el middleware

--- Backend/src/routers/middelware.ts
+++ Backend/src/routers/middelware.ts
@@ -4,10 +4,11 @@ import { rutaProtegida } from '../controllers/Inicio/rutaProtected'
 import { routerUsuario } from '../controllers/usuario/usuario'
 import { tareasUsuario } from '../controllers/tareas/tareasUsuario'
 import { rutaVerifi } from '../controllers/Inicio/verify'
-
+import { routerRecuperar } from '../controllers/Inicio/recuperar'
 export const midelware = Router()
 
 midelware.use('/api', routerLogin)
+midelware.use('/api', routerRecuperar)
+midelware.use('/api', rutaVerifi)
 midelware.use('/api', rutaProtegida, routerUsuario)
 midelware.use('/api', rutaProtegida, tareasUsuario)
-midelware.use('/api', rutaVerifi)
