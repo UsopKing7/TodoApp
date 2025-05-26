b651369402281cd9c1079dd04e11793f9d6e63bc
UsopKing7 <programadorusop@gmail.com>
2025-05-26 17:49:38 -0400

    reemplaza el contenido de la página de tareas con la estructura de encabezado y secciones para mostrar tareas y descripciones

--- Frontend/src/pages/tareas/tareas.tsx
+++ Frontend/src/pages/tareas/tareas.tsx
@@ -1,5 +1,26 @@
+import '../../styles/tareas.css'
+import { FaBars } from 'react-icons/fa'
+
 export const Tareas = () => {
   return (
-    <h1>Hello word</h1>
+    <>
+      <header className="cabezera-header">
+        <FaBars size={32} className="icono-menu" />
+        <h1 className="titulo-todo-app">TodoApp - UsopKing</h1>
+      </header>
+
+      <main className="contenedor-tareas">
+        <section className="tarjeta tareas-todo">
+          <h2 className="titulo-seccion">Tus Tareas</h2>
+          <ul className="lista-tareas">
+            <li>📝 Aprender React</li>
+          </ul>
+        </section>
+
+        <section className="tarjeta descripcion-todo">
+          <h2 className="titulo-seccion">Descripción</h2>
+        </section>
+      </main>
+    </>
   )
 }
