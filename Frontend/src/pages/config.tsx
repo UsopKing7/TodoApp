24cd2e20098012ed7d91a39174a31b8b50ff984b
UsopKing7 <programadorusop@gmail.com>
2025-05-27 04:52:12 -0400

    agrega la función useCerrarCession para manejar el cierre de sesión y la navegación

--- Frontend/src/pages/config.tsx
+++ Frontend/src/pages/config.tsx
@@ -1,4 +1,5 @@
 import { useNavigate } from 'react-router-dom'
+import { BASE_URL } from '../config'
 
 export const useVolverForm = () => {
   const navigate = useNavigate()
@@ -8,4 +9,24 @@ export const useVolverForm = () => {
     navigate(-1)
   }
   return { volver }
+}
+
+export const useCerrarCession = () => {
+  const navigate = useNavigate()
+
+  const cerrarSession = async () => {
+    const res = await fetch(`${BASE_URL}/api/logout`, {
+      method: 'POST',
+      credentials: 'include'
+    })
+  
+    if (res.ok) {
+      alert('Session cerrada Correctamente')
+      navigate('/')
+    } else {
+      alert('Error al cerrar session')
+    }
+  }
+
+  return { cerrarSession }
 }
