311141b8924bc4a9610d09124d53c26035a1fb41
UsopKing7 <programadorusop@gmail.com>
2025-05-27 01:51:53 -0400

    agrega el routerId al middleware para gestionar rutas de identificación

--- Backend/src/routers/middelware.ts
+++ Backend/src/routers/middelware.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express'
-import { routerLogin } from '../controllers/Inicio/login'
+import { routerLogin, routerId } from '../controllers/Inicio/login'
 import { rutaProtegida } from '../controllers/Inicio/rutaProtected'
 import { routerUsuario } from '../controllers/usuario/usuario'
 import { tareasUsuario } from '../controllers/tareas/tareasUsuario'
@@ -7,6 +7,7 @@ import { rutaVerifi } from '../controllers/Inicio/verify'
 import { routerRecuperar } from '../controllers/Inicio/recuperar'
 export const midelware = Router()
 
+midelware.use('/api', routerId)
 midelware.use('/api', routerLogin)
 midelware.use('/api', routerRecuperar)
 midelware.use('/api', rutaVerifi)
